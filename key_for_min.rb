2052034f2e827d1c5963016020667d9e0eaf3cae
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 05:23:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
     new_value_array<<value
   end
   new_value_array.sort
+  put "new_value_array"
   smallest_value=new_value_array[0]
 
   return name_hash.key(smallest_value)
